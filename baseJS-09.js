9956384d295d823d3c0df805ee791b2743e34e1a
Amanbay Kenesary <koni9812@gmail.com>
2024-06-06 20:18:49 +0500

    wi

---
+++ baseJS-09.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = 'Kenesary'
+const myStr = 'My name is ' + myName + ' and i am well!'
---
+++ baseJS-10.js
@@ -0,0 +1,4 @@
+// Change code below this line
+const someAdjective = 'fyp'
+let myStr = 'Learning to code is '
+myStr += someAdjective
