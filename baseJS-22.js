8d671b4106558063a9f11676f41e15322ae9eea2
Amanbay Kenesary <koni9812@gmail.com>
2024-06-07 13:35:57 +0500

    win

---
+++ baseJS-22.js
@@ -0,0 +1,9 @@
+//Modify the data stored at index 0 of myArray to a value of 45.
+
+//Solution:
+
+// Setup
+const myArray = [18, 64, 99]
+
+// Only change code below this line
+myArray[0] = 45
---
+++ baseJS-24.js
@@ -0,0 +1,12 @@
+//Push ["dog", 3] onto the end of the myArray variable.
+
+//Solution:
+
+// Setup
+const myArray = [
+  ['John', 23],
+  ['cat', 2],
+]
+
+// Only change code below this line
+myArray.push(['dog', 3])
