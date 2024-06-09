281a8145d6e73019fd0b9307afdc49a230f77d0d
Amanbay Kenesary <koni9812@gmail.com>
2024-06-10 00:02:25 +0500

    night

---
+++ baseJS-28.js
@@ -0,0 +1,10 @@
+//There should be at least 5 sub-arrays in the list.
+//Solution:
+
+const myList = [
+  ['Chocolate Bar', 17],
+  ['Chocolate Bar', 17],
+  ['Chocolate Bar', 17],
+  ['Chocolate Bar', 17],
+  ['Chocolate Bar', 17],
+]
---
+++ baseJS-30.js
@@ -0,0 +1,10 @@
+//Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+//Call the function with two numbers as arguments.
+//Solution:
+
+function functionWithArgs(sum1, sum2) {
+  console.log(sum1 + sum2)
+}
+
+functionWithArgs(1, 2)
+functionWithArgs(7, 9)
