a558b7d0f8ba3128a783d67e5dca5b2994c1c24f
Amanbay Kenesary <koni9812@gmail.com>
2024-06-09 14:07:42 +0500

    dinner

---
+++ baseJS-26.js
@@ -0,0 +1,12 @@
+//Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+//Solution:
+
+// Setup
+const myArray = [
+  ['John', 23],
+  ['dog', 3],
+]
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift()
